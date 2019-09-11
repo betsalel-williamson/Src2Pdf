f674306187256bf6cb46ef0702fc82d3395883ef
Betsalel (Saul) Williamson <misc@betsalel.me>
2019-09-10 20:00:59 -0400

    updated latex template and examples.

--- example/v2/folder with space at end/js-file.js
+++ example/v2/folder with space at end/js-file.js
@@ -1,2 +1,6 @@
 console.log( 'Hello, world!' );
 // JUST FOR THE HECK OF IT! I'LL CHANGE TOO :-)
+var foo = "some \" string with a quote\' ";
+
+// the regex delimeters should be highlighted differently
+var regex = foo.toLowerCase().replace(/[ ,.\/!@#$%\^&*\(\)\-_,+=|\\\{\}\[\]\`~:;\"\'<>?]{1,}/g,'-');
--- example/v2/folder with space at end/tcl-file.tcl
+++ example/v2/folder with space at end/tcl-file.tcl
@@ -4,3 +4,6 @@ puts "Hello World"
 # example comment
 
 puts "A very long line of text.  A very long line of text.  A very long line of text.  A very long line of text.  A very long line of text.  A very long line of text.  A very long line of text.  A very long line of text.  A very long line of text.  A very long line of text.  A very long line of text.  A very long line of text."
+
+# the escaped quotes should not end the quote
+set cmd "latexdiff-vc --pdf \"$prevRoot/$inline\" \"$curRoot/$inline2\""
